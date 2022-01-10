f7a77dbb3f3dce94fadc925c0d8c8b66f129c825
Rajashekar Chintalapati <rajashekar.chintalapati@walmart.com>
2022-01-09 17:39:19 -0800

    Refactor: performance improvements

--- indicate/hindi2english.py
+++ indicate/hindi2english.py
@@ -24,7 +24,12 @@ class HindiToEnglish(Base):
     START_TOKEN = "^"
     END_TOKEN = "$"
 
-    model = None
+    weights_loaded = False
+
+    input_lang_tokenizer = None
+    target_lang_tokenizer = None
+    encoder = None
+    decoder = None
 
     @classmethod
     def transliterate(cls, input, latest=False):
@@ -36,51 +41,58 @@ class HindiToEnglish(Base):
             output (str): English text
         """
 
-        if cls.model is None:
+        if not cls.weights_loaded:
             cls.model_path, cls.input_vocab, cls.target_vocab = cls.load_model_data(latest)
 
-        input_lang_tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(cls.input_vocab)
-        target_lang_tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(cls.target_vocab)
+            cls.input_lang_tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(cls.input_vocab)
+            cls.target_lang_tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(cls.target_vocab)
 
-        vocab_inp_size = len(input_lang_tokenizer.word_index) + 1
-        vocab_tar_size = len(target_lang_tokenizer.word_index) + 1
+            vocab_inp_size = len(cls.input_lang_tokenizer.word_index) + 1
+            vocab_tar_size = len(cls.target_lang_tokenizer.word_index) + 1
 
-        encoder = Encoder(vocab_inp_size, cls.embedding_dim, cls.units, cls.BATCH_SIZE)
-        decoder = Decoder(
-            vocab_tar_size,
-            cls.embedding_dim,
-            cls.units,
-            cls.BATCH_SIZE,
-            cls.max_length_input,
-            cls.max_length_output,
-            "luong",
-        )
-        optimizer = tf.keras.optimizers.Adam()
-        logger.debug(f"checking weights for encoder {encoder.embedding.variables}")
-        logger.debug(f"checking weights for decoder {decoder.embedding.variables}")
+            cls.encoder = Encoder(vocab_inp_size, cls.embedding_dim, cls.units, cls.BATCH_SIZE)
+            cls.decoder = Decoder(
+                vocab_tar_size,
+                cls.embedding_dim,
+                cls.units,
+                cls.BATCH_SIZE,
+                cls.max_length_input,
+                cls.max_length_output,
+                "luong",
+            )
+            logger.debug(f"checking weights for encoder {cls.encoder.embedding.variables}")
+            logger.debug(f"checking weights for decoder {cls.decoder.embedding.variables}")
 
-        # This is needed to load waits from checkpoint
-        example_input_batch = tf.random.uniform(shape=[64, 47])
-        sample_hidden = encoder.initialize_hidden_state()
-        sample_output, sample_h, sample_c = encoder(example_input_batch, sample_hidden)
-        sample_x = tf.random.uniform((cls.BATCH_SIZE, cls.max_length_output))
-        decoder.attention_mechanism.setup_memory(sample_output)
-        initial_state = decoder.build_initial_state(cls.BATCH_SIZE, [sample_h, sample_c], tf.float32)
-        decoder(sample_x, initial_state)
+            # This is needed to load waits from checkpoint
+            example_input_batch = tf.random.uniform(shape=[64, 47])
+            sample_hidden = cls.encoder.initialize_hidden_state()
+            sample_output, sample_h, sample_c = cls.encoder(example_input_batch, sample_hidden)
+            sample_x = tf.random.uniform((cls.BATCH_SIZE, cls.max_length_output))
+            cls.decoder.attention_mechanism.setup_memory(sample_output)
+            initial_state = cls.decoder.build_initial_state(cls.BATCH_SIZE, [sample_h, sample_c], tf.float32)
+            cls.decoder(sample_x, initial_state)
 
-        logger.debug(f"checking weights for encoder after random input {encoder.embedding.variables}")
-        logger.debug(f"checking weights for decoder after random input {decoder.embedding.variables}")
+            logger.debug(f"checking weights for encoder after random input {cls.encoder.embedding.variables}")
+            logger.debug(f"checking weights for decoder after random input {cls.decoder.embedding.variables}")
 
-        logger.debug(f"Restoring model weights from {cls.model_path}")
-        # restore weights from checkpoint
-        encoder.load_weights(f"{cls.model_path}/encoder")
-        decoder.load_weights(f"{cls.model_path}/decoder")
-        logger.debug(f"encoder weights after restoring from checkpoint {encoder.embedding.variables}")
-        logger.debug(f"decoder weights after restoring from checkpoint {decoder.embedding.variables}")
+            logger.debug(f"Restoring model weights from {cls.model_path}")
+            # restore weights from checkpoint
+            cls.encoder.load_weights(f"{cls.model_path}/encoder")
+            cls.decoder.load_weights(f"{cls.model_path}/decoder")
+            logger.debug(f"encoder weights after restoring from checkpoint {cls.encoder.embedding.variables}")
+            logger.debug(f"decoder weights after restoring from checkpoint {cls.decoder.embedding.variables}")
+
+            cls.weights_loaded = True
 
         # Predictions
         target = translate(
-            input, cls.units, input_lang_tokenizer, target_lang_tokenizer, encoder, decoder, cls.max_length_input
+            input,
+            cls.units,
+            cls.input_lang_tokenizer,
+            cls.target_lang_tokenizer,
+            cls.encoder,
+            cls.decoder,
+            cls.max_length_input,
         )
         logger.debug(f"Model predicted {target}")
 
